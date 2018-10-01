f03f2b3da004402b511718971ed5482a8c1e78b4
Thanarak.PIR <thanarack@pirsquare.ne>
2018-10-01 15:38:59 +0700

    Update config path

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-const UserKey = 'fc6YazAyQkWverz52EaKDtJv3S3q9hHEzKMR8'
+const UserKey = 'fc6YazAyQkWverz52EaKDtJv3S3q9hHEzKMR8';
 
 export const Config = {
   port: 3333,
@@ -6,43 +6,49 @@ export const Config = {
   // Auth configuration
   userAuth: 'username',
   userKey: UserKey,
-	saltRound: 10,
+  saltRound: 10,
   // Upload configuration
   ImagePath: './upload',
   // Reset password configuration
-	resetpass: {
-		host: 'http://localhost',
-		amount: 24,
-		unit: 'hours'
-	},
-	email: {
-		provider: {
-			service: 'Gmail',
-			host: 'smtp.gmail.com',
-			port: 587,
-			secure: true,
-			auth: {
-				user: 'dev.mail@gmail.com',
-				pass: 'devpassword!'
-			}
-		},
-		from: 'dev@yourhost.name'
+  resetpass: {
+    host: 'http://localhost',
+    amount: 24,
+    unit: 'hours',
   },
-  // database configuration
-	database: {
-		prod: {
-			host: 'production_host',
-			username: 'production_username',
-			password: 'production_password',
-			port: 3306,
-			database: 'production_databasename'
-		},
-		dev: {
-			host: 'localhost',
-			username: 'root',
-			password: '',
-			port: 3306,
-			database: 'nestjs'
-		}
-	}
-}
+  email: {
+    provider: {
+      service: 'Gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: true,
+      auth: {
+        user: 'dev.mail@gmail.com',
+        pass: 'devpassword!',
+      },
+    },
+    from: 'dev@yourhost.name',
+  },
+  // Dir & Path
+  path: {
+    preFixApi: 'api',
+    upload: '/upload',
+    static: __dirname + '/../upload',
+  },
+  // Database configuration
+  database: {
+    prod: {
+      host: 'production_host',
+      username: 'production_username',
+      password: 'production_password',
+      port: 3306,
+      database: 'production_databasename',
+    },
+    dev: {
+      host: 'localhost',
+      username: 'root',
+      password: '',
+      port: 3306,
+      database: 'nestjs',
+    },
+  },
+};
