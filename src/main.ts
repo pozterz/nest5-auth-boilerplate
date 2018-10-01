8280bf19cbf7f99351eeead6de05ea3fedada3c8
Thanarak.PIR <thanarack@pirsquare.ne>
2018-10-01 16:15:47 +0700

    Update money format

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,34 @@
 import { NestFactory, FastifyAdapter } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Config } from './config/'
+import { Config } from './config/';
 import * as helmet from 'helmet';
-import * as express from 'express'
-import * as bodyParser from 'body-parser'
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 // import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.setGlobalPrefix(Config.path.preFixApi)
-  app.use(Config.path.upload, express.static(Config.path.static))
-  app.use(bodyParser.json({ limit: '3MB' }))
+  app.setGlobalPrefix(Config.path.preFixApi);
+  app.use(Config.path.upload, express.static(Config.path.static));
+  app.use(bodyParser.json({ limit: '3MB' }));
   app.use(helmet());
-  app.enableCors()
+  app.enableCors();
   // uncomment here if you want to use API rate limit and install express-rate-limit
   // app.use(rateLimit({
   //   windowMs: 15 * 60 * 1000, // 15 minutes
   //   max: 100 // limit each IP to 100 requests per windowMs
   // }));
   const options = new DocumentBuilder()
-        .setBasePath('/api')
-        .setTitle('APIs')
-        .setDescription('APIs Documentation')
-        .setVersion('1.0')
-        .addTag('v1')
-        .build();
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('/docs', app, document);
+    .setBasePath('/api')
+    .setTitle('APIs')
+    .setDescription('APIs Documentation')
+    .setVersion('1.0')
+    .addTag('v1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
   await app.listen(Config.port);
 }
 bootstrap();
--- src/utils/utils.functions.ts
+++ src/utils/utils.functions.ts
@@ -1,46 +1,57 @@
 export const setDate = (date, type) => {
-	return type == 'start'
-		? new Date(new Date(date).setHours(0, 0, 0, 0)).toLocaleString()
-		: new Date(new Date(date).setHours(23, 59, 59, 59)).toLocaleString()
-}
+  return type == 'start'
+    ? new Date(new Date(date).setHours(0, 0, 0, 0)).toLocaleString()
+    : new Date(new Date(date).setHours(23, 59, 59, 59)).toLocaleString();
+};
 
 export const paginate = async (model, options, generatedQuery) => {
-	let limit = options.limit ? options.limit : 25
-	let page, offset, skip, promises, include
+  let limit = options.limit ? options.limit : 25;
+  let page, offset, skip, promises, include;
 
-	if (options.offset) {
-		offset = options.offset
-		skip = offset
-	} else if (options.page) {
-		page = options.page
-		skip = (page - 1) * limit
-	} else {
-		page = 1
-		offset = 0
-		skip = offset
-	}
+  if (options.offset) {
+    offset = options.offset;
+    skip = offset;
+  } else if (options.page) {
+    page = options.page;
+    skip = (page - 1) * limit;
+  } else {
+    page = 1;
+    offset = 0;
+    skip = offset;
+  }
 
-	let docsQuery = {}
-	if (limit) {
-		generatedQuery['offset'] = skip
+  let docsQuery = {};
+  if (limit) {
+    generatedQuery['offset'] = skip;
 
-		docsQuery = await model.findAndCountAll(generatedQuery)
-		let result = {
-			data: docsQuery['rows'],
-			total: docsQuery['count'],
-			limit: limit
-		}
-		if (offset !== undefined) {
-			result['offset'] = offset
-		}
-		console.log('count', docsQuery['count'])
-		if (page !== undefined) {
-			result['page'] = page
-			result['pages'] = Math.ceil(Number(docsQuery['count']) / limit) || 1
-		}
+    docsQuery = await model.findAndCountAll(generatedQuery);
+    let result = {
+      data: docsQuery['rows'],
+      total: docsQuery['count'],
+      limit: limit,
+    };
+    if (offset !== undefined) {
+      result['offset'] = offset;
+    }
+    console.log('count', docsQuery['count']);
+    if (page !== undefined) {
+      result['page'] = page;
+      result['pages'] = Math.ceil(Number(docsQuery['count']) / limit) || 1;
+    }
 
-		return result
-	} else {
-		return await model.findAndCountAll(generatedQuery)
-	}
-}
+    return result;
+  } else {
+    return await model.findAndCountAll(generatedQuery);
+  }
+};
+
+export const moneyFormat = (price, sign = '$') => {
+  const pieces = parseFloat(price)
+    .toFixed(2)
+    .split('');
+  let ii = pieces.length - 3;
+  while ((ii -= 3) > 0) {
+    pieces.splice(ii, 0, ',');
+  }
+  return sign + pieces.join('');
+};
